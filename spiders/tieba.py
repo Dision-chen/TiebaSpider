513e39e89c2b2555dff91d96ce25df76bfdd10f2
Dision <964746175@qq.com>
2021-01-25 17:35:17 +0800

    贴吧爬虫

---
+++ spiders/tieba.py
@@ -0,0 +1,40 @@
+import scrapy
+from TiebaSpider.items import TiebaspiderItem
+
+
+class TiebaSpider(scrapy.Spider):
+    name = 'tieba'
+    allowed_domains = ['tieba.baidu.com']
+    start_urls = ['http://tieba.baidu.com/mo/q----,sz@320_240-1-3---2/m?kw=林允儿&pn=0']
+
+    def parse(self, response):
+        div_list = response.xpath("//div[contains(@class,'i')]")
+        for div in div_list:
+            item = TiebaspiderItem()
+            item["title"] = div.xpath(".//a/text()").extract_first()
+            item["href"] = "http://tieba.baidu.com/mo/q----,sz@320_240-1-3---2/" + div.xpath(".//a/@href").extract_first()
+            yield scrapy.Request(
+                item["href"],
+                callback=self.parse_detail,
+                meta={"item": item}
+            )
+        next_url = response.xpath("//a[text()='下一页']/@href").extract_first()
+        if next_url is not None:
+            yield scrapy.Request(
+                "http://tieba.baidu.com/mo/q----,sz@320_240-1-3---2/" + next_url,
+                callback=self.parse)
+
+
+    def parse_detail(self, response):
+        item = response.meta["item"]
+        div_list = response.xpath("//div[@class='i']")
+        for div in div_list:
+            item["content"] = div.xpath("./text()").extract_first()
+            item["content_img"] = div.xpath(".//img/@src").extract()
+            yield item
+        next_url = response.xpath("//a[text()='下一页']/@href").extract_first()
+        if next_url is not None:
+             yield scrapy.Request(
+                 "http://tieba.baidu.com/mo/q----,sz@320_240-1-3---2/" + next_url,
+                 callback=self.parse_detail)
+
